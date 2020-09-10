90dd3175d720642b6d95d3997997dabbd16d939e
Jakob Buis <jakob.buis@in10.nl>
2020-09-10 11:59:16 +0200

    code formatting PSR-1 and PSR-2

--- src/Normalizer/LinkNormalizer.php
+++ src/Normalizer/LinkNormalizer.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Drupal\link_normalizer\Normalizer;
 
 use Drupal\Core\Url;
@@ -9,27 +10,27 @@ use Drupal\serialization\Normalizer\NormalizerBase;
  */
 class LinkNormalizer extends NormalizerBase
 {
-  /**
-   * The interface or class that this Normalizer supports.
-   *
-   * @var string
-   */
-  protected $supportedInterfaceOrClass = 'Drupal\Core\TypedData\TypedDataInterface';
+    /**
+     * The interface or class that this Normalizer supports.
+     *
+     * @var string
+     */
+    protected $supportedInterfaceOrClass = 'Drupal\Core\TypedData\TypedDataInterface';
 
-  /**
-   * {@inheritdoc}
-   */
-  public function normalize($object, $format = null, array $context = array())
-  {
-    $value = $object->getValue();
-    $parent = $object->getParent();
-    $type = $parent->getFieldDefinition()->getType();
-    if ($type === 'link') {
-      $internalLink = $parent->get('uri')->getValue();
-      $link = Url::fromUri($internalLink, array('absolute' => false))->toString();
-      $object->getParent()->get('uri')->setValue('placeholder');
-      $value = ['url' => $link];
+    /**
+     * {@inheritdoc}
+     */
+    public function normalize($object, $format = null, array $context = array())
+    {
+        $value = $object->getValue();
+        $parent = $object->getParent();
+        $type = $parent->getFieldDefinition()->getType();
+        if ($type === 'link') {
+            $internalLink = $parent->get('uri')->getValue();
+            $link = Url::fromUri($internalLink, array('absolute' => false))->toString();
+            $object->getParent()->get('uri')->setValue('placeholder');
+            $value = ['url' => $link];
+        }
+        return $value;
     }
-    return $value;
-  }
 }
